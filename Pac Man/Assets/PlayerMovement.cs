28572d72a025196c0ac74ae5fc12ba12b8be1fc4
AJones58 <70922658+AJones58@users.noreply.github.com>
2021-02-04 09:12:22 -0600

    Character Movement
    I created the basic character movement.

---
+++ Pac Man/Assets/PlayerMovement.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public CharacterController chomp;
+    Vector3 velocity;
+    public float speed = 12f;
+    public float gravity = -9.81f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+        Vector3 move = transform.right * x + transform.forward * z;
+        chomp.Move(move * speed * Time.deltaTime);
+        chomp.Move(velocity * Time.deltaTime);
+
+        velocity.y += gravity * Time.deltaTime;
+
+           
+    }
+}
