d39df7664f1b75e418124b514c7459c8d1e0538a
Nathan Lege <69705751+Brelius@users.noreply.github.com>
2021-02-04 10:07:17 -0600

    Main Menu and scripts
    Added scripts folder as well as the menu script which controls the menu
    set up start menu
    set up victory screen
    set up game over screen

---
+++ Pac Man/Assets/Scripts/MenuScript.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class MenuScript : MonoBehaviour
+{
+    // this script is made for the main menu and switching between scnes in game 
+    // also will deal with the stats page if that is implemented 
+    void Start()
+    {
+        // keeps cusor in the play area 
+        Cursor.visible = true;
+        Cursor.lockState = CursorLockMode.None;
+    }
+
+    // Will cause the application to exit when called through the quit game button 
+    public void QuitGame()
+    {
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#else
+            Application.Quit(); 
+#endif
+    }
+    // will start the game through the start game button 
+    public void StartGame()
+    {
+        SceneManager.LoadScene("In_Game"); 
+    }
+}
